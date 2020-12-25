6a5525d9175940785fc81ef1d5b9663a3facfea5
Andrew Smithson <smithson@uk.ibm.com>
2020-12-25 19:38:40 +0000

    Fix missing test

--- day25/day25_test.go
+++ day25/day25_test.go
@@ -12,3 +12,8 @@ func TestCalcLoop(t *testing.T) {
 	require.Equal(t, 8, CalcLoop(cardpub))
 	require.Equal(t, 11, CalcLoop(17807724))
 }
+
+func TestEncrypt(t *testing.T) {
+	require.Equal(t, 14897079, Encrypt(17807724, 8))
+	require.Equal(t, 14897079, Encrypt(5764801, 11))
+}
